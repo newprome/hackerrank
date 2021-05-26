0d3f755171bf6d3a9c7b33ae01faab15207d032c
newprome <67219704+newprome@users.noreply.github.com>
2021-05-26 13:26:02 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,18 @@
+// Digit Frequency
+
+// Input Format
+
+// The first line contains a string, num which is the given number.
+
+// Constraints
+
+// All the elements of num are made of english alphabets and digits.
+
+// Output Format
+
+// Print ten space-separated integers in a single line denoting the frequency of each digit from 0 to 9.
+
+
+
+
+
